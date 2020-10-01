d524bf46dc85e671974cad9befed0be908006770
Erica <ericasu33@gmail.com>
2020-10-01 17:19:25 +0000

    created same function but with return instead of console.log

--- hello.js
+++ hello.js
@@ -3,3 +3,12 @@ const sayHello = name => {
 }
 
 sayHello("Erica");
+
+//return
+
+const returnSayHello = name => {
+ return `Hello, ${name}`;
+}
+
+const greetings = returnSayHello("Erica");
+console.log(greetings);
