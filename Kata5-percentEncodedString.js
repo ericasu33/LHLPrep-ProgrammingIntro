62fc68a5f84cd96a28e2643b373e8f0ee47a6cf7
Erica <ericasu33@gmail.com>
2020-10-06 01:29:42 +0000

    removed the .trim() and used for and if only to do the same

--- Kata5-percentEncodedString.js
+++ Kata5-percentEncodedString.js
@@ -1,4 +1,5 @@
 //replace spacing with %20
+// uses .trim();
 
 const urlEncode = (str) => {
   let word = "";
@@ -15,6 +16,41 @@ const urlEncode = (str) => {
   return word;
 }
 
+//replace spacing with %20
+//without using .trim()
+
+const urlEncode = (str) => {
+  let word = "";
+  let index1 = 0;
+  let index2 = 0;
+
+  //finding index of the actual first letter
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== " "){
+      index1 = i ;
+      break;
+    } 
+  }
+
+  //finding index of the actual last letter
+  for (let i = str.length - 1; i >= 0; i--) {
+    if (str[i] !== " "){
+      index2 = i ;
+      break;
+    } 
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== " "){
+      word += str[i];
+    } else if (str[i] === " " && i >= index1 && i <= index2){
+      word += "%20";
+    }
+  }  
+   return word;
+}
+
+
 console.log(urlEncode("Lighthouse Labs"));
-console.log(urlEncode("    Lighthouse Labs "));
+console.log(urlEncode(" Lighthouse Labs "));
 console.log(urlEncode("blue is greener than purple for sure"));
