5fdaa77a15cb503d0767bff555a2214e5f4e7571
Erica <erica@Ericas-MacBook-Pro.local>
2020-10-26 20:43:32 -0700

    refractored code

--- Loopy-Lighthouse2-S.js
+++ Loopy-Lighthouse2-S.js
@@ -3,19 +3,33 @@
 // 2 - multiples (arr of 2 numbers rep multiples we want to replace wz words)
 // 3 - arr of 2 strings rep word that will replace multiples
 
+// const loopyLighthouse = (range, multiples, words) => {
+//   for (let i = range[0]; i <= range[1]; i++) {
+//     const m1 = i % multiples[0] === 0 ;
+//     const m2 = i % multiples[1] === 0 
+//     if (m1 && m2) {
+//       console.log(words.join(""));
+//     } else if (m1) {
+//       console.log(words[0]);
+//     } else if (m2) {
+//       console.log(words[1]);
+//     } else {
+//       console.log(i);
+//     }
+//   }
+// };
+
 const loopyLighthouse = (range, multiples, words) => {
   for (let i = range[0]; i <= range[1]; i++) {
-    const m1 = i % multiples[0] === 0 ;
-    const m2 = i % multiples[1] === 0 
-    if (m1 && m2) {
-      console.log(words.join(""));
-    } else if (m1) {
-      console.log(words[0]);
-    } else if (m2) {
-      console.log(words[1]);
-    } else {
-      console.log(i);
+    let output = "";
+
+    if (i % multiples[0] === 0) {
+      output += words[0];
+    } 
+    if (i % multiples[1] === 0) {
+      output += words[1];
     }
+    console.log(output === "" ? i : output);
   }
 };
 
